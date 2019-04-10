00de076c3291b25713ca19d21f4e52d352a73302
Alison Hill <apreshill@gmail.com>
2019-04-10 11:27:10 -0700

    spin the .R script

--- 2019-04-09/tidy_tuesday_4_9_19.R
+++ 2019-04-09/tidy_tuesday_4_9_19.R
@@ -1,6 +1,8 @@
-# Tidy Tuesday 4/9/2019
-# Allison Horst
-# Tennis Grand Slam Champions
+#' ---
+#' title: Tidy Tuesday 4/9/2019<br> Tennis Grand Slam Champions
+#' author: Allison Horst
+#' output: github_document
+#' ---
 
 library(tidyverse)
 library(RColorBrewer)
@@ -11,14 +13,14 @@ library(LaCroixColoR)
 
 # font_import()
 
-# Get data:
-# player_dob <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/player_dob.csv")
+#' Get data:
 
-# grand_slams <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slams.csv")
+#+ message = FALSE
+player_dob <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/player_dob.csv")
+grand_slams <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slams.csv")
+grand_slam_timeline <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slam_timeline.csv")
 
-# grand_slam_timeline <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slam_timeline.csv")
-
-# This seems like excessive grouping and ungrouping, mer?
+#' This seems like excessive grouping and ungrouping, mer?
 
 past_qual <- c("Won","Finalist","Semi-finalist","Quarterfinalist", "4th Round", "3rd Round", "2nd Round", "1st Round")
 
@@ -30,13 +32,13 @@ sum_table <- grand_slam_timeline %>%
   group_by(gender) %>%
   group_split() # This is pretty cool! First time using group_split()
 
-# Access the separate tibbles (probably bad practice, but wanted to try using group_splitanyway...)
+#' Access the separate tibbles (probably bad practice, but wanted to try using group_splitanyway...)
 
 male_players <- sum_table[[2]]
 female_players <- sum_table[[1]]
 
 
-# Top 20 males by # appearances after qualification (doesn't include absence/retire data)
+#' Top 20 males by # appearances after qualification (doesn't include absence/retire data)
 top_male_appear <- male_players %>%
   group_by(player) %>%
   summarize(
@@ -45,7 +47,7 @@ top_male_appear <- male_players %>%
   arrange(-appearances) %>%
   head(20)
 
-# Top females by # appearances after qualification (doesn't include absence/retire data)
+#' Top females by # appearances after qualification (doesn't include absence/retire data)
 top_female_appear <- female_players %>%
   group_by(player) %>%
   summarize(
@@ -57,7 +59,7 @@ top_female_appear <- female_players %>%
 vec_m <- unique(top_male_appear$player)
 vec_f <- unique(top_female_appear$player)
 
-# Joins to keep top 10 by total appearances beyond qualifying round
+#' Joins to keep top 10 by total appearances beyond qualifying round
 
 m_appear <- top_male_appear %>%
   inner_join(male_players) %>%
@@ -69,22 +71,23 @@ f_appear <- top_female_appear %>%
   mutate(player = as.factor(player)) %>%
   mutate(outcome = as.factor(outcome))
 
-# Relevel by top players
+#' Relevel by top players
 f_appear$player <- fct_relevel(f_appear$player, vec_f)
 m_appear$player <- fct_relevel(m_appear$player, vec_m)
 
-# Relevel outcome
+#' Relevel outcome
 f_appear$outcome <- fct_relevel(f_appear$outcome,"Won", "Finalist","Semi-finalist","Quarterfinalist","4th Round", "3rd Round","2nd Round","1st Round")
 
 m_appear$outcome <- fct_relevel(m_appear$outcome,"Won", "Finalist","Semi-finalist","Quarterfinalist","4th Round", "3rd Round","2nd Round","1st Round")
 
-# Then make a graph that shows the level reached after qualifiers for each:
+#' Then make a graph that shows the level reached after qualifiers for each:
 
+#' Create palette:
 # pal <- wes_palette(8, name = "FantasticFox1", type = "continuous")
-# Create palette:
 pal <- lacroix_palette("PassionFruit", n = 8, type = "continuous")
 
-# Graph of female top appearances:
+#' Graph of female top appearances:
+#+ dpi = 250
 ggplot(f_appear, aes(x = reorder(player, desc(player)), y = n)) +
   geom_col(aes(fill = outcome)) +
   theme_pomological(base_family = "Courier New", base_size = 12) +
@@ -95,10 +98,11 @@ ggplot(f_appear, aes(x = reorder(player, desc(player)), y = n)) +
   theme(legend.position = "bottom", axis.text.x=element_text(size=11, face = "bold", hjust = 1), axis.text.y = element_text(size = 10, color = "slateblue4")) +
   coord_flip()
 
-# Save it:
+#' Save it:
 ggsave("my_tennis_plot.png", width = 8, height = 7)
 
-# Male version:
+#' Male version:
+#+ dpi = 250
 ggplot(m_appear, aes(x = reorder(player, desc(player)), y = n)) +
   geom_col(aes(fill = outcome)) +
   theme_pomological(base_family = "Courier New",
